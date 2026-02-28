c642854999927e4ea87634d74d2f0817ad8c923b
oussema karia <bro188411@gmail.com>
2026-02-28 21:09:08 +0100

    v9.5

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import { Navbar } from "./components/layout/Navbar";
 import { Footer } from "./components/layout/Footer";
+import { Helmet, HelmetProvider } from 'react-helmet-async'; // Importation SEO
+
+// Importation des pages
 import Index from "./pages/Index";
 import Projets from "./pages/Projets";
-import ProjetDetail from "./pages/ProjetDetail";
 import Temoignages from "./pages/Temoignages";
 import Equipe from "./pages/Equipe";
 import Competences from "./pages/Competences";
@@ -21,48 +23,55 @@ import FormacityDetail from "./pages/FormacityDetail";
 import CafeZoneProject from "./pages/cafeDetail";
 import WebFusionDetail from "./pages/WebFusionDetail";
 import JNBFitnessDetail from "./pages/JNBFitnessDetail";
-
 import TeamSection from "./pages/TeamSection";
 import Parlez from "./pages/Parlez";
 import Services from "./pages/Services";
-  
 
 const queryClient = new QueryClient();
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <div className="min-h-screen flex flex-col">
-          <Navbar />
-          <AnimatePresence mode="wait">
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/projets" element={<Projets />} />
-              <Route path="/projets/mylb" element={<MyLBDetail />} />
-              <Route path="/projets/formacity" element={<FormacityDetail />} />
-              <Route path="/projets/cafe-zone" element={<CafeZoneProject />} />
-              <Route path="/projets/webfusion-digital" element={<WebFusionDetail />} />
-              <Route path="/projets/jnbfitness" element={<JNBFitnessDetail />} />
-              <Route path="/temoignages" element={<Temoignages />} />
-              <Route path="/gallery" element={<TeamSection />} />
-              <Route path="/equipe" element={<Equipe />} />
-              <Route path="/Services" element={<Services />} />
-              <Route path="/competences" element={<Competences />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/parlez" element={<Parlez />} />
-              <Route path="/Admin067" element={<AdminLogin />} />
-              <Route path="/Admin067/panel" element={<AdminPanel />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </AnimatePresence>
-          <Footer />
-        </div>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <HelmetProvider> 
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        {/* Configuration SEO par défaut pour tout le site */}
+        <Helmet>
+            <title>WebFusion | Agence Digitale & Solutions Web</title>
+            <meta name="description" content="WebFusion accompagne les entreprises dans leur transformation digitale avec des solutions web modernes et performantes." />
+            <link rel="canonical" href="https://webfusiondigital.tn" />
+        </Helmet>
+
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <div className="min-h-screen flex flex-col">
+            <Navbar />
+            <AnimatePresence mode="wait">
+              <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="/projets" element={<Projets />} />
+                <Route path="/projets/mylb" element={<MyLBDetail />} />
+                <Route path="/projets/formacity" element={<FormacityDetail />} />
+                <Route path="/projets/cafe-zone" element={<CafeZoneProject />} />
+                <Route path="/projets/webfusion-digital" element={<WebFusionDetail />} />
+                <Route path="/projets/jnbfitness" element={<JNBFitnessDetail />} />
+                <Route path="/temoignages" element={<Temoignages />} />
+                <Route path="/gallery" element={<TeamSection />} />
+                <Route path="/equipe" element={<Equipe />} />
+                <Route path="/Services" element={<Services />} />
+                <Route path="/competences" element={<Competences />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/parlez" element={<Parlez />} />
+                <Route path="/Admin067" element={<AdminLogin />} />
+                <Route path="/Admin067/panel" element={<AdminPanel />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </AnimatePresence>
+            <Footer />
+          </div>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  </HelmetProvider>
 );
 
 export default App;
